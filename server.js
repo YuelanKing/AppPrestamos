f5f98a3d06496b3c2ea17d1be81d20efecdce03c
Alexis Hevia <hevia.alexis@gmail.com>
2012-08-08 21:35:58 -0500

    Ahora se muestra un mensaje de error cuando la conexión a mongoDB falla

--- server.js
+++ server.js
@@ -1,52 +1,57 @@
+var express = require('express'), 
+    routes = require('./routes'), 
+    mongoose = require('mongoose');
 
-/**
- * Module dependencies.
- */
+// Start app after connecting to MongoDB
+mongoose.connection.on('open', function (ref) {
+  console.log('Connected to mongo server.');
 
-var express = require('express')
-  , routes = require('./routes')
-  , mongoose = require('mongoose');
+  var app = express.createServer();
+  
+  // Configuration
+  app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser());
+    app.use(express.session({ secret: 'your secret here' }));
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+  });
 
-var app = module.exports = express.createServer();
+  app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  });
+
+  app.configure('production', function(){
+    app.use(express.errorHandler());
+  });
+
+  // Routes
+
+  //app.get('/', routes.hello);
+
+  //CREATE
+  app.post('/loan', routes.loans_create);
+
+  //READ
+  app.get('/loans/:id', routes.loans_read);
+
+  //UPATE
+  app.put('/loans/:id', routes.loans_update);
+
+  //LIST LOANS
+  app.get('/loans', routes.loans);
+
+  app.listen(3000, function(){
+    console.log("Express server listening on port 3000 in %s mode", app.settings.env);
+  });
+});
+
+mongoose.connection.on('error', function (err) {
+  console.log('Could not connect to mongo server!');
+  console.log(err);
+});
 
 mongoose.connect('mongodb://127.0.0.1:27017/AppPrestamos');
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: 'your secret here' }));
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-// Routes
-
-//app.get('/', routes.hello);
-
-//CREATE
-app.post('/loan', routes.loans_create);
-
-//READ
-app.get('/loans/:id', routes.loans_read);
-
-//UPATE
-app.put('/loans/:id', routes.loans_update);
-
-//LIST LOANS
-app.get('/loans', routes.loans);
-
-app.listen(3000, function(){
-  console.log("Express server listening on port 3000 in %s mode", app.settings.env);
-});
